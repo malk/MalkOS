ecfe62af01bd4cdb36fa00856aaaab760cbe4d4c
Malk’Zameth <m@zameth.org>
2014-08-05 15:57:20 +0200

    MalkOS starts by searching if a newer version of itself is available and using it if it is.

--- dwim.sh
+++ dwim.sh
@@ -9,13 +9,26 @@ command -v git >/dev/null 2>&1 || {
 mkdir -p /opt
 cd /opt
 
+current_script_is_latest=false
+
 if [ -d MalkOS ]
 then
     cd MalkOS
-    git pull origin master
+    git fetch origin master
+    if [ git diff --quiet origin/master ]
+    then
+	current_script_is_latest=true
+    else
+	git merge origin master
+    fi
 else
     git clone http://github.com/malk/MalkOS.git
     cd MalkOS
+fi
+
+if [ ! $current_script_is_latest ]
+then
+    exec dwim.sh
 fi
 
 command -v ansible-playbook >/dev/null 2>&1 || {
