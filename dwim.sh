ea6bad956de3377365d3b735dc565a5f54097df7
Malk’Zameth <m@zameth.org>
2014-08-16 15:05:24 +0200

    changed dwim to just use ansible-pull instead of re-inveting the wheel

--- dwim.sh
+++ dwim.sh
@@ -6,39 +6,11 @@ command -v git >/dev/null 2>&1 || {
     apt-get -y install git
 }
 
-mkdir -p /opt
-cd /opt
-
-current_script_is_latest=false
-
-if [ -d MalkOS ]
-then
-    cd MalkOS
-    git fetch origin master
-    if [ git diff --quiet origin/master ]
-    then
-	current_script_is_latest=true
-    fi
-    cd /opt
-fi
-
-if [ ! $current_script_is_latest ]
-then
-    rm -rf MalkOS
-    git clone http://github.com/malk/MalkOS.git
-    exec ./MalkOS/dwim.sh
-fi
-
-cd MalkOS
-
-# either is installed or is not
-# if installed it is either the latest or not
-# if installed and latest just keep going
-# else reinstall
-
-command -v ansible-playbook >/dev/null 2>&1 || {
+command -v ansible-pull >/dev/null 2>&1 || {
     apt-get -y install software-properties-common
-    apt-add-repository ppa:ansible/ansible
+    apt-add-repository -y ppa:ansible/ansible
     apt-get -y update
     apt-get -y install ansible
 }
+
+ansible-pull --directory=/opt/MalkOS/ --url=http://github.com/malk/MalkOS.git
